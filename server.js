f9fbe6ec85269e5cd973ddfd81a030138bb1b340
Pawan Arora <pawan.arora@voicetree.co>
2019-07-17 19:20:31 +0530

    ws connect send and receive msg

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const websocket = require('ws');
+
+const server = http.createServer((req, res) => {
+    res.end("I am connected");
+});
+const wss = new websocket.Server({ server });
+
+wss.on('headers', (headers, req) => {
+   // console.log(headers); Not logging the header anymore
+});
+
+//Event: 'connection'
+wss.on('connection', (ws, req) => {
+    ws.send('This is a message from server, connection is established');
+    //receive the message from client on Event: 'message'
+    ws.on('message', (msg) => {
+        console.log(msg);
+    });
+});
+
+server.listen(8000);
